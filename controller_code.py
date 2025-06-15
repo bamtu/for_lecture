d370aad3e46bfbab6ec773f64dd444c7fae9b33b
Jeong Dae Kim <59360@naver.com>
2025-06-15 20:41:25 +0900

    update

---
+++ controller_code.py
@@ -0,0 +1,34 @@
+def on_right_released():
+    radio.send_number(0)
+controller.right.on_event(ControllerButtonEvent.RELEASED, on_right_released)
+
+def on_down_released():
+    radio.send_number(0)
+controller.down.on_event(ControllerButtonEvent.RELEASED, on_down_released)
+
+def on_left_released():
+    radio.send_number(0)
+controller.left.on_event(ControllerButtonEvent.RELEASED, on_left_released)
+
+def on_up_released():
+    radio.send_number(0)
+controller.up.on_event(ControllerButtonEvent.RELEASED, on_up_released)
+
+number = 30
+radio.set_group(number)
+
+def on_forever():
+    if controller.up.is_pressed():
+        radio.send_string("A1")
+    if controller.down.is_pressed():
+        radio.send_string("A2")
+    if controller.left.is_pressed():
+        radio.send_string("A3")
+    if controller.right.is_pressed():
+        radio.send_string("A4")
+    if controller.A.is_pressed():
+        radio.send_string("A5")
+    if controller.B.is_pressed():
+        radio.send_string("A6")
+        
+forever(on_forever)
